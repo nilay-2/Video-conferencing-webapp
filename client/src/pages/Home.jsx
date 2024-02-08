e1b9d37ddac24129b84673f9df93fe270c52dcaa
Nilay Pophalkar <nilaypophalkar@gmail.com>
2024-02-09 00:56:52 +0530

    updated padding of landing page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -69,19 +69,19 @@ const Home = () => {
   };
 
   return (
-    <div className="h-screen w-screen bg-landing_page bg-no-repeat bg-cover p-5">
+    <div className="h-screen w-screen bg-landing_page bg-no-repeat bg-cover">
       <div
         className="h-full w-full mx-auto flex flex-col"
         style={{ maxWidth: "1900px" }}
       >
         <div className="w-full header max-w-7xl mx-auto">
-          <p className="text-blue-600 text-3xl font-extrabold md:pl-6 md:pt-6">
+          <p className="text-blue-600 text-3xl font-extrabold pl-6 pt-6">
             <i className="bi bi-person-video3"></i>
             <span className="ml-3">ZoomZest</span>
           </p>
         </div>
         <div className="h-full mt-16">
-          <div className="text-content-container max-w-7xl mx-auto flex md:flex-row justify-between flex-col">
+          <div className="text-content-container px-5 max-w-7xl mx-auto flex md:flex-row justify-between flex-col">
             <div className="md:order-1 order-2">
               <div className="text-content mt-28 flex flex-col md:gap-10 gap-12">
                 <p className="sm:text-3xl text-2xl font-semibold text-slate-700 sm:max-w-md">
